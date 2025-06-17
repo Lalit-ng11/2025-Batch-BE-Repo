3dc8fed1e3d30ddab06a3e92fbbaf030ce02cda5
Lalit-ng11 <lalitpatil.itp@gmail.com>
2025-06-17 11:16:43 +0530

    These files contains data upto update operation.

--- route.js
+++ route.js
@@ -2,6 +2,7 @@
 const route  = require("express").Router();
 const UserModule = require('./module');
 
+// Register User 
 route.post('/register',async (req,res)=>{
 
     const newUser = new UserModule({
@@ -13,10 +14,36 @@ route.post('/register',async (req,res)=>{
         const SaveData = await newUser.save();
         res.send(SaveData);
     } catch (error) {
-        console.log(`Registration Faile...${error}`);
+        console.log(`Registration Failed...${error}`);
         
     }
 })
+
+// Show Data of Users 
+route.get('/showdata', async (req,res)=>{
+    try {
+        const showData = await UserModule.find();
+        res.send(showData)
+    } catch (error) {
+        console.log(`Show Data Failed...${error}`);
+    }
+})
+
+// Delete User Data 
+route.delete('/delete', async (req,res)=>{
+
+    let id = req.query.id;
+    try {
+        const deleteData = await UserModule.findByIdAndDelete(id);
+        // res.send(deleteData);
+        res.send("Data Deleted Successfully..!");
+    } catch (error) {
+        console.log('Delete data Failed.',error);
+          
+    }
+})
+
+
 module.exports = route;
 
 
